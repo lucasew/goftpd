c5b7b9d58325cd5c36f03713ec7c80d71fe38327
lucas59356 <lucas59356@gmail.com>
2017-10-31 14:01:09 -0200

    Migrating log library to github.com/lucas59356/go-logger

--- main.go
+++ main.go
@@ -2,45 +2,53 @@ package main
 
 import (
 	"flag"
-	"log"
+	logger "github.com/lucas59356/go-logger"
 	"net/http"
 	"net/url"
 	"os"
+	"errors"
 )
 
 var (
 	port     = flag.String("p", "80", "Onde eu vou escutar")
 	basepath = flag.String("d", "./", "Root folder")
+	// ErrFolderNotFound When the working folder does not exist
+	ErrFolderNotFound = errors.New("A pasta de trabalho não existe")
+	// ErrCantParseURL When server can't parse the URL
+	ErrCantParseURL = errors.New("Não foi possivel parsear a url")
 )
 
 func main() {
+	log := logger.New("main")
 	flag.Parse()
 	if !isPastaExist(*basepath) { // Será que essa pasta existe?
-		log.Fatal("ERRO: A pasta de trabalho não existe: " + *basepath)
+		log.Error(ErrFolderNotFound)
 		flag.Usage()
 	}
 	bindTo := "0.0.0.0:" + *port
-	log.Println("Iniciando servidor...")
+	log.Info("Iniciando servidor...")
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", server)                                   // Handler do file server
-	log.Printf("Meu trabalho acontecerá na pasta:  " + *basepath) // Estou lendo na pasta!!
-	log.Printf("Entendido, vamos trabalhar em %s", bindTo)        // Estou bindando!!
-	log.Println("Pau na máquina!")
+	log.Info("Meu trabalho acontecerá na pasta: %s", *basepath) // Estou lendo na pasta!!
+	log.Info("Entendido, vamos trabalhar em %s", bindTo)        // Estou bindando!!
+	log.Info("Pau na máquina!")
 
 	err := http.ListenAndServe(bindTo, mux)
 	if err != nil {
-		log.Fatal("ERRO: " + err.Error())
+		log.Error(err)
 	}
 }
 
 func server(w http.ResponseWriter, r *http.Request) {
+	log := logger.New("srv")
 	var path = *basepath + r.RequestURI  // Junta endereço da pasta com a url pedida
 	path, err := url.QueryUnescape(path) // Remover urlencode
 	if err != nil {
-		log.Println("ERRO: Não foi possivel parsear a url: ", path)
+		log.Error(ErrCantParseURL)
+		log.Error(err)
 	}
-	log.Printf("[%s] %s %s", r.Host, r.Method, r.URL) // Debug, e tbm por que é mais legal :v
+	log.Debug("[%s] %s %s", r.Host, r.Method, r.URL) // Debug, e tbm por que é mais legal :v
 	http.ServeFile(w, r, path)
 	w.Header().Set("Cache-Control", "max-age=5") // Evitar que o cache do navegador bugue a listagem, tirando a necessidade do ^F5
 }
